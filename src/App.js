5b10a6ab4d9c1d606127a1697db32d18714674bf
Vladimir RM <shvdrummer@gmail.com>
2023-07-19 15:37:11 +0300

    Created component PostItem.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React,{useState} from 'react';
-import './App.css';
+import "./styles/App.css"
 
 function App() {
 
   const [value,setValue] = useState('')
   return (
     <div className="App">
-      <div className='post'>
-        <div className="post__content">
-          <strong>1 Java script</strong>
-          <div>JS lengvige</div>
-        </div>
-        </div>
-        <div className="post__btns">
-          <button>Delete Post</button>
-        </div>
+
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const PostItem = () => {
+  return (
+
+            <div className='post'>
+        <div className="post__content">
+          <strong>1 Java script</strong>
+          <div>JS lengvige</div>
+        </div>
+        <div className="post__btns">
+          <button>Delete Post</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default PostItem
