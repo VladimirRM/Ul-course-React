2abd4b9a7673a7232aab4118a7174987a7412995
Vladimir RM <shvdrummer@gmail.com>
2023-07-20 08:07:12 +0300

    Added title for post LIst.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import "./styles/App.css"
-import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 function App() {
 const [posts,setPosts] = useState([
@@ -11,14 +11,8 @@ const [posts,setPosts] = useState([
 
   return (
     <div className="App">
-      <h1 style={{textAlign: 'center'}}>Posts List</h1>
-     {posts.map((post)=>(
-      <PostItem
-      post={post}
-      key={post.id}  
-      />
-
-     ))}
+     <PostList 
+     posts={posts}title='Post list 1'/>
    
 
     </div>
