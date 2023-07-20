4549dccb78458c3079180a4e2a9b47eef31674d1
Vladimir RM <shvdrummer@gmail.com>
2023-07-20 07:59:02 +0300

    Add component PostList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React,{useState} from 'react';
 import "./styles/App.css"
+import PostItem from './components/PostItem';
 
 function App() {
+const [posts,setPosts] = useState([
+  {id:1, title: 'Hello', body: 'description'},
+  {id:2, title: 'Hello', body: 'description'},
+  {id:3, title: 'Hello', body: 'description'},
+])
 
-  const [value,setValue] = useState('')
   return (
     <div className="App">
+      <h1 style={{textAlign: 'center'}}>Posts List</h1>
+     {posts.map((post)=>(
+      <PostItem
+      post={post}
+      key={post.id}  
+      />
+
+     ))}
+   
 
     </div>
   );
