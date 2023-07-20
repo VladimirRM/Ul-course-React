40b995e931290f4b9f3e2b98c55f9d1ce66cf7a6
Vladimir RM <shvdrummer@gmail.com>
2023-07-20 09:22:45 +0300

    Created controlled input and start useRef.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useRef, useState} from 'react';
 import "./styles/App.css"
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
@@ -11,12 +11,21 @@ const [posts,setPosts] = useState([
   {id:3, title: 'Hello', body: 'description'},
 ])
 
+const [title,setTitle] = useState('')
 
+const addNewPost = (e)=>{
+ e.preventDefault()  
+}
+
+const bodyInputRef = useRef()
 
 
   return (
     <div className="App">
-      <MyInput type="text" placeholder='Name post' />
+      <MyInput type="text" placeholder='Name post' 
+       value={title}
+       onChange={e=>setTitle(e.target.value)}
+      />
       <MyInput type="text" placeholder='Description'/>
       <MyButton onClick={addNewPost} >Add post</MyButton>
      <PostList posts={posts}title='Post about Js'/>
