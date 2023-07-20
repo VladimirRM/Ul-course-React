89d1022966c5677e9bd9c77dc42b70981e84ddf1
Vladimir RM <shvdrummer@gmail.com>
2023-07-20 11:34:39 +0300

    Add const newPost.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React,{ useState} from 'react';
 import "./styles/App.css"
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+
+
+import PostForm from './components/PostForm';
 
 function App() {
 const [posts,setPosts] = useState([
@@ -12,32 +13,21 @@ const [posts,setPosts] = useState([
   {id:3, title: 'Hello', body: 'description'},
 ])
 
-const [post,setPost] = useState({title:'', body:''})
-
-
-const addNewPost = (e)=>{
-  e.preventDefault()
-
-setPosts([...posts,{...post,id:Date.now()}])
-setPost({title:'', body:''})
-
+const createPost = (newPost)=>{
+     setPosts([...posts,newPost])
 }
+
+
+
+
 
 
 
 
   return (
     <div className="App">
-      <MyInput type="text" placeholder='Name post' 
-       value={post.title}
-       onChange={e=>setPost({...post, title: e.target.value})}
-      />
 
-      <MyInput type="text" placeholder='Description'
-     value={post.body}
-     onChange={e=>setPost({...post, body: e.target.value})}
-      />
-      <MyButton onClick={addNewPost} >Add post</MyButton>
+      <PostForm create={createPost}/>
      <PostList posts={posts}title='Post about Js'/>
 
    
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from 'react'
+import MyButton from './UI/button/MyButton'
+import MyInput from './UI/input/MyInput'
+
+
+const PostForm = () => {
+
+const [post,setPost] = useState({title:'', body:''})
+
+const addNewPost = (e)=>{
+    e.preventDefault()
+    const newPost = {
+        ...post,id:Date.now()
+    }  
+
+  setPost({title:'', body:''})
+  
+  }
+
+  return (
+    <div>
+            <form>
+      <MyInput type="text" placeholder='Name post' 
+       value={post.title}
+       onChange={e=>setPost({...post, title: e.target.value})}
+      />
+
+      <MyInput type="text" placeholder='Description'
+     value={post.body}
+     onChange={e=>setPost({...post, body: e.target.value})}
+      />
+      <MyButton onClick={addNewPost} >Add post</MyButton>
+      </form>
+    </div>
+  )
+}
+
+export default PostForm
