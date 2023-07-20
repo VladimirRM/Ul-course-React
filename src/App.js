bcea100a6af85b8686c9e216524fd73f4aac7187
Vladimir RM <shvdrummer@gmail.com>
2023-07-20 19:05:42 +0300

    Added useMemo.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React,{ useState} from 'react';
+import React,{ useState,useMemo} from 'react';
 import "./styles/App.css"
 import PostList from './components/PostList';
 
 
 import PostForm from './components/PostForm';
 import MySelect from './components/UI/MySelect/MySelect';
+import MyInput from './components/UI/input/MyInput'
 
 function App() {
 const [posts,setPosts] = useState([
 
-  {id:1, title: 'Hello', body: 'description'},
-  {id:2, title: 'Hello', body: 'description'},
-  {id:3, title: 'Hello', body: 'description'},
+  {id:1, title: 'dd', body: 'vvption'},
+  {id:2, title: 'aallo', body: 'description'},
+  {id:3, title: 'ccllo', body: 'aescription'},
 ])
 const [selectedSort, setSelectedSort]= useState('')
+const [searchQuery, setSearchQuery]= useState('')
+
+
+function  getSortedPosts(){
+  console.log("sorted done")
+     if(selectedSort){
+      return [...posts].sort((a,b)=> a[selectedSort].localeCompare(b[selectedSort]))
+     }
+     return posts
+}
+
+const sortedPosts = getSortedPosts()
 
 const createPost = (newPost)=>{
      setPosts([...posts,newPost])
@@ -21,15 +34,26 @@ const createPost = (newPost)=>{
 const removePost = (post)=>{
      setPosts(posts.filter(p=> p.id !== post.id))
 }
-// const removePost = (post,id)=>{
-//      setPosts([...posts].filter(post=> post.id !== id))
-// }
+
+const sortPosts = (sort)=>{
+console.log(sort)
+setSelectedSort(sort);
+
+}
 return (
     <div className="App">
      <PostForm create={createPost}/>
+     <hr style={{margin: '15px'}} />
      <div>
+      <MyInput
+      value={searchQuery}
+      onChange={(e)=>setSearchQuery(e.target.value)}
+      placeholder="Search..."
+      />
       <MySelect
-      defaultValue='Sort by'
+      value={selectedSort}
+      onChange={sortPosts}
+      defaultValue='Select'
       options={[
         {value: 'title',name: 'ON name'},
         {value: 'body',name: 'ON discription'}
@@ -40,7 +64,7 @@ return (
 
      </div>
      {posts.length !== 0
-      ?  <PostList posts={posts}title='Post about Js'remove={removePost}/>
+      ?  <PostList posts={sortedPosts}title='Post about Js'remove={removePost}/>
       :<h1 style={{textAlign: 'center'}}>Posts not found!</h1>
     }
     
