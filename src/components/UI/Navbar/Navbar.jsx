3913ae67386654d78b41f7ec1cb23c8912bb2b1e
Vladimir RM <shvdrummer@gmail.com>
2023-07-23 19:26:27 +0300

    Add button exit for the navbar.

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import "../../../styles/App.css"
+import MyButton from '../button/MyButton'
 
 
 const Navbar = () => {
   return (
-    <div>
         <div className="navbar">
+        <MyButton>Exit</MyButton>
+
         <div className="navbar__link">
       <Link to="/about">About</Link>
       <Link to="/posts">Posts</Link>
-    </div>
+
    
   </div>
    
