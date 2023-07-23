328b6bc42769b9b9e46ab46933e435f588081904
Vladimir RM <shvdrummer@gmail.com>
2023-07-23 08:22:51 +0300

    Add component approuter.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css'
 import About from './pages/About';
 import Posts from './pages/Posts';
-
+import Error from './pages/Error';
 import "./styles/App.css"
 import {BrowserRouter,Route,Switch,Redirect} from 'react-router-dom'
 import Navbar from './components/UI/Navbar/Navbar';
@@ -18,7 +18,10 @@ function App() {
   <Route path='/posts'>
     <Posts />
   </Route>
-    <Redirect  to='/posts'/>
+  <Route path='/error'>
+    <Error />
+  </Route>
+    <Redirect  to='/error'/>
   </Switch>
 
   </BrowserRouter>
